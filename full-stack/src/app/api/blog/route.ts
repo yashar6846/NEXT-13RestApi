6f2d2c007ce2ace5189933baa47e0759c6ab42b0
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-08-17 19:41:47 +0300

    add put the project

--- full-stack/src/app/api/blog/route.ts
+++ full-stack/src/app/api/blog/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "../../../../prisma";
 
 
-async function main() {
+export async function main() {
     try{
     await prisma.$connect();
     }catch(err){
