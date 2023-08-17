c6f1c66cdd6f5421f7e632bc518f91ff2ae7a649
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-08-17 19:10:36 +0300

    add ferstfile nextja

---
+++ full-stack/src/app/api/blog/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../../prisma";
+
+
+async function main() {
+    try{
+    await prisma.$connect();
+    }catch(err){
+        throw new Error("Database Connection Unsuccessful");
+    }
+}
+
+export const GET = async(req : Request, res : NextResponse)=>{
+   try{
+    await main();
+    const posts = await prisma.post.findMany()
+     return NextResponse.json({ message: "Success", posts}, { status: 200})
+   } catch(err){
+    return NextResponse.json({ message: "Error", error: err}, { status: 500 })
+   } finally {
+    await prisma.$disconnect()
+   }
+}
+
+export const POST = async(req : Request, res : NextResponse)=>{
+    try{
+        const {title, description} = await req.json();
+     await main();
+     const post = await prisma.post.create({data: {description, title}})
+      return NextResponse.json({ message: "Success", post}, { status: 201})
+    } catch(err){
+     return NextResponse.json({ message: "Error", error: err}, { status: 500 })
+    } finally {
+     await prisma.$disconnect()
+    }
+ }
