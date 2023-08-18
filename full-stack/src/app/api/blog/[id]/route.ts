725f1a9658366e837c03c950fcc61731b17dddc5
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-08-18 10:58:43 +0300

    add delete and put

--- full-stack/src/app/api/blog/[id]/route.ts
+++ full-stack/src/app/api/blog/[id]/route.ts
@@ -19,22 +19,32 @@ export const GET = async (req: Request, res: NextResponse) =>{
 
 
 export const PUT = async (req: Request, res: NextResponse) =>{
-
     try{
-  const id = req.url.split("/blog/")[1]
-  await main()
-    }catch(err){
-        throw new Error("")
-    }
-
+        const id = req.url.split("/blog/")[1]
+        const {title, description}= await req.json()
+        await main()
+        const post = await prisma.post.update({
+        data: {title, description},
+        where: {id}
+         })
+        return NextResponse.json({message: "Success", post}, {status: 200})
+          }catch(err){
+            return NextResponse.json({message: "Error", err}, {status: 500})
+          } finally{
+            await prisma.$disconnect()
+          }
 }
 
+ 
 export const DELETE = async (req: Request, res: NextResponse) =>{
-
     try{
-
-    }catch(err){
-        throw new Error("")
-    }
-
+        const id = req.url.split("/blog/")[1]
+        await main()
+       const post = await prisma.post.delete({where:{id}})
+        return NextResponse.json({message: "Success", post}, {status: 200})
+          }catch(err){
+            return NextResponse.json({message: "Error", err}, {status: 500})
+          } finally{
+            await prisma.$disconnect()
+          }
 }
